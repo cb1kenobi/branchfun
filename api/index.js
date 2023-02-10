b5c787915e29bcf519a74d9d16c3f55d76e6a298
Chris Barber <chris.barber@vercel.com>
2023-02-10 08:11:26 -0600

    Change branch name

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 export default async function handler(req, res) {
-	res.status(200).end(`Branch: main API_KEY=${process.env.API_KEY || 'No API key set!!'} >> ${new Date().toISOString()}\n`);
+	res.status(200).end(`Branch: bangbang API_KEY=${process.env.API_KEY || 'No API key set!!'} >> ${new Date().toISOString()}\n`);
 }
